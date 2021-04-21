3c10348fba2a6112e308d6864a7d1db7c7418e4c
ElinaPark <elinapark0818@gmail.com>
2021-04-21 23:41:22 +0900

    filter

---
+++ Algorithm/filter.js
@@ -0,0 +1,41 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+function solution(answers) {
+    var answer = [];
+    const man1 = [1, 2, 3, 4, 5];
+    const man2 = [2, 1, 2, 3, 2, 4, 2, 5];
+    const man3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    let count = [0, 0, 0];
+
+    for(let i = 0; i < answers.length; i++) {
+        if(answers[i] == man1[i % man1.length]) count[0]++;
+        if(answers[i] == man2[i % man2.length]) count[1]++;
+        if(answers[i] == man3[i % man3.length]) count[2]++;
+    }
+
+    const max = Math.max(count[0], count[1], count[2]);
+    for(let i = 0; i < count.length; i++) {
+        if(max == count[i]) answer.push(i + 1);
+    }
+
+    return answer;
+}
