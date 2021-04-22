a406e98dba77e5c39b75581ce5780a8bfab7b3bd
ElinaPark <elinapark0818@gmail.com>
2021-04-23 00:34:37 +0900

    에라토스테네스의 체 개념 이해

--- Algorithm/Sieve_of_Eratosthenes.js
+++ Algorithm/Sieve_of_Eratosthenes.js
@@ -3,32 +3,28 @@ function sieve(n) {
     for (let i = 0; i < n + 1; i += 1) {
         arr.push(true);
     }
-
     for (let i = 2; i * i <= n; i += 1) {
         if (arr[i]) {
-            for(let j = i * i; j <= n; j += 1) {
+            for (let j = i * i; j <= n; j += 1) {
                 arr[j] = false;
             }
         }
     }
-
     arr.splice(0, 2, false, false);
-
     const result = arr.filter((value) => {
         return value !== false;
     })
-
     return result.length;
 }
 
 // 다른 방법
 
 let solution = (n) => {
-    let arr = Array(n+1).fill(true).fill(false, 0, 2);
+    let arr = Array(n + 1).fill(true).fill(false, 0, 2);
 
-    for(let i=2; i*i<=n; i++){
-        if(arr[i]){
-            for(let j=i*i; j<=n; j+=i){
+    for (let i = 2; i * i <= n; i++) {
+        if (arr[i]) {
+            for (let j = i * i; j <= n; j += i) {
                 arr[j] = false;
             }
         }
@@ -36,3 +32,4 @@ let solution = (n) => {
 
     return arr.filter(e => e).length;
 }
+
