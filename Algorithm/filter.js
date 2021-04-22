6a6d11feaf0b81a59a8c1635ca93a8357a037caa
ElinaPark <elinapark0818@gmail.com>
2021-04-22 13:49:13 +0900

    Web-APIs window-scroll

--- Algorithm/filter.js
+++ Algorithm/filter.js
@@ -1,26 +1,6 @@
 
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
 function solution(answers) {
-    var answer = [];
+    const answer = [];
     const man1 = [1, 2, 3, 4, 5];
     const man2 = [2, 1, 2, 3, 2, 4, 2, 5];
     const man3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
