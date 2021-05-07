6192cb2fe193005eb2f98d0d3b6e853c5d062a57
ElinaPark <elinapark0818@gmail.com>
2021-05-07 22:51:28 +0900

    이벤트 위임 성능개선(메모리)

--- todolist/main.js
+++ todolist/main.js
@@ -4,9 +4,6 @@ const items = document.querySelector('.items');
 const input = document.querySelector('.footer__text');
 const plusBtn = document.querySelector('.plusBtn');
 
-
-
-
 function onText() {
     const text = input.value;
     if (text === '') {
@@ -25,29 +22,37 @@ function onText() {
 function createItem(text) {
     const itemRow = document.createElement('li');
     itemRow.setAttribute('class', 'item__row');
-
-    const item = document.createElement('div');
-    item.setAttribute('class', 'item');
-
-    const name = document.createElement('span');
-    name.setAttribute('class', 'item__name');
-    name.innerText = text;
-
-    const deleteBtn = document.createElement('button');
-    deleteBtn.setAttribute('class', 'item__delete');
-    deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
-    deleteBtn.addEventListener('click', () => {
-        items.removeChild(itemRow);
-    })
-
-    const itemDivider = document.createElement('div');
-    itemDivider.setAttribute('class', 'item__divider');
-
-    item.appendChild(name);
-    item.appendChild(deleteBtn);
-
-    itemRow.appendChild(item);
-    itemRow.appendChild(itemDivider);
+    itemRow.innerHTML = `
+    <div class="item">
+        <span class="item__name"></span>
+        <button class="item__delete">
+            <i class="fas fa-trash-alt"></i>
+        </button>
+    </div>
+    <div class="item__divider"></div>
+    `;
+    // const item = document.createElement('div');
+    // item.setAttribute('class', 'item');
+    //
+    // const name = document.createElement('span');
+    // name.setAttribute('class', 'item__name');
+    // name.innerText = text;
+    //
+    // const deleteBtn = document.createElement('button');
+    // deleteBtn.setAttribute('class', 'item__delete');
+    // deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
+    // deleteBtn.addEventListener('click', () => {
+    //     items.removeChild(itemRow);
+    // })
+    //
+    // const itemDivider = document.createElement('div');
+    // itemDivider.setAttribute('class', 'item__divider');
+    //
+    // item.appendChild(name);
+    // item.appendChild(deleteBtn);
+    //
+    // itemRow.appendChild(item);
+    // itemRow.appendChild(itemDivider);
     return itemRow;
 }
 
