6346c37c737ca42dd5b3f97e3671a3a810c96a01
ElinaPark <elinapark0818@gmail.com>
2021-05-07 23:31:02 +0900

    :zap: reset button

--- todolist/main.js
+++ todolist/main.js
@@ -19,40 +19,20 @@ function onText() {
     input.focus();
 }
 
+let id = 0; // UUID...best!
 function createItem(text) {
     const itemRow = document.createElement('li');
     itemRow.setAttribute('class', 'item__row');
+    itemRow.setAttribute('data-id', id);
     itemRow.innerHTML = `
-    <div class="item">
-        <span class="item__name"></span>
-        <button class="item__delete">
-            <i class="fas fa-trash-alt"></i>
-        </button>
-    </div>
-    <div class="item__divider"></div>
-    `;
-    // const item = document.createElement('div');
-    // item.setAttribute('class', 'item');
-    //
-    // const name = document.createElement('span');
-    // name.setAttribute('class', 'item__name');
-    // name.innerText = text;
-    //
-    // const deleteBtn = document.createElement('button');
-    // deleteBtn.setAttribute('class', 'item__delete');
-    // deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
-    // deleteBtn.addEventListener('click', () => {
-    //     items.removeChild(itemRow);
-    // })
-    //
-    // const itemDivider = document.createElement('div');
-    // itemDivider.setAttribute('class', 'item__divider');
-    //
-    // item.appendChild(name);
-    // item.appendChild(deleteBtn);
-    //
-    // itemRow.appendChild(item);
-    // itemRow.appendChild(itemDivider);
+            <div class="item">
+                <span class="item__name">${text}</span>
+                <button class="item__delete">
+                    <i class="fas fa-trash-alt" data-id=${id}></i>
+                </button>
+            </div>
+            <div class="item__divider"></div>`;
+    id++;
     return itemRow;
 }
 
@@ -66,9 +46,16 @@ input.addEventListener('keypress', event => {
     }
 });
 
+items.addEventListener('click', event => {
+    const id = event.target.dataset.id;
+    if(id) {
+        const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+        toBeDeleted.remove();
+    }
+});
 
 const resetBtn = document.querySelector('.resetBtn');
 
-resetBtn.addEventListener('click', () => {
-    document.querySelector('items').reset();
+resetBtn.addEventListener('click',() => {
+    items.innerHTML = '';
 })
