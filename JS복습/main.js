516341f7adaeed2c58f3b871abb248d09bcfced5
ElinaPark <elinapark0818@gmail.com>
2021-05-10 17:44:49 +0900

    const coolCounter = new Counter(); // undefined 여서 TypeError 발생한다

--- JS복습/main.js
+++ JS복습/main.js
@@ -74,7 +74,7 @@ function alertNum(num) {
     alert(`alert! 5배수! ${num}`)
 }
 
-const coolCounter = new Counter(alertNum);
+const coolCounter = new Counter(); // undefined 여서 TypeError 발생한다
 
 coolCounter.increase();
 coolCounter.increase();
