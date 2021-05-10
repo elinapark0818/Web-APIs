9182337e7ece5e1609f169a9ebfd755967ed16e4
ElinaPark <elinapark0818@gmail.com>
2021-05-10 17:16:27 +0900

    콜백함수(runIf5Times)로 전달해서 호출, 전달하기

--- JS복습/main.js
+++ JS복습/main.js
@@ -56,19 +56,23 @@ class Counter {
         this.counter = 0;
     }
 
-    increase() {
+    increase(runIf5Times) {
         this.counter++;
         console.log(this.counter);
         if (this.counter % 5 === 0) {
-            console.log('5배수 입니다!')
+            runIf5Times();
         }
     }
 }
 
-
 const coolCounter = new Counter();
-coolCounter.increase();
-coolCounter.increase();
-coolCounter.increase();
-coolCounter.increase();
-coolCounter.increase();
+
+function printSomething() {
+    console.log('5배수입니다!!!')
+}
+
+coolCounter.increase(printSomething);
+coolCounter.increase(printSomething);
+coolCounter.increase(printSomething);
+coolCounter.increase(printSomething);
+coolCounter.increase(printSomething);
