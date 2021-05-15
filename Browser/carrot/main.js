be4b795535a74c5d1704371217efc111d166a64e
ElinaPark <elinapark0818@gmail.com>
2021-05-15 17:30:15 +0900

    feat: 재생버튼 클릭 시, bgm play, 타이머 끝나면 bgm pause

--- Browser/carrot/main.js
+++ Browser/carrot/main.js
@@ -2,6 +2,19 @@
 
 const gameBtn = document.querySelector('.game__btn');
 const gameTimer = document.querySelector('.game__timer');
+const gameField = document.querySelector('.game__field');
+const gameScore = document.querySelector('.game__score');
+const refreshBtn = document.querySelector('.pop-up__refresh');
+const gameMessage = document.querySelector('.pop-up__message');
+
+// sound
+const bgm = new Audio("./sound/bg.mp3");
+const alertSound = new Audio("./sound/alert.wav");
+const bugSound = new Audio("./sound/bug_pull.mp3");
+const carrotSound = new Audio("./sound/carrot_pull.mp3");
+const winSound = new Audio("./sound/game_win.mp3");
+
+
 function changeBtn() {
     document.querySelector('.fa-play').className = "fas fa-stop";
 }
@@ -13,16 +26,13 @@ function createImage() {
 }
 
 
-const gameField = document.querySelector('.game__field');
-const gameScore = document.querySelector('.game__score');
-const refreshBtn = document.querySelector('.pop-up__refresh');
-const gameMessage = document.querySelector('.pop-up__message');
 
 
 function timer(sec) {
     let timerId = setInterval(() => {
         gameTimer.innerText = `0: ${sec}`
         if (sec <= 0) {
+            bgm.pause();
             clearInterval(timerId);
             return;
         }
@@ -34,4 +44,5 @@ gameBtn.addEventListener('click', () => {
     changeBtn()
     timer(9)
     createImage()
+    bgm.play();
 })
