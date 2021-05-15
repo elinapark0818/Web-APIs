c055448ea07120cb5e9305eeee1c967476c09b86
ElinaPark <elinapark0818@gmail.com>
2021-05-15 18:47:08 +0900

    feat: 아이템(당근, 벌레) 랜덤 위치 출력

--- Browser/carrot/main.js
+++ Browser/carrot/main.js
@@ -8,24 +8,30 @@ const refreshBtn = document.querySelector('.pop-up__refresh');
 const gameMessage = document.querySelector('.pop-up__message');
 
 // sound
-const bgm = new Audio("./sound/bg.mp3");
+const bgSound = new Audio("./sound/bg.mp3");
 const alertSound = new Audio("./sound/alert.wav");
 const bugSound = new Audio("./sound/bug_pull.mp3");
 const carrotSound = new Audio("./sound/carrot_pull.mp3");
 const winSound = new Audio("./sound/game_win.mp3");
+
+// replace
+const items = document.querySelector('.items');
+
+let itemsWidth = items.clientWidth;
+let itemsHeight = items.clientHeight;
+let carrots = document.querySelectorAll(".carrot");
+let bugs = document.querySelectorAll(".bug");
 
 
 function changeBtn() {
     document.querySelector('.fa-play').className = "fas fa-stop";
 }
 
-function createImage() {
-    const images = new Image(50,50);
-    images.src = "img/bug.png";
-    document.querySelector('.game__field').appendChild(images)
-}
-
-
+// function createImage() {
+//     const images = new Image(50, 50);
+//     images.src = "img/bug.png";
+//     document.querySelector('.game__field').appendChild(images)
+// }
 
 
 function timer(sec) {
@@ -41,8 +47,20 @@ function timer(sec) {
 }
 
 gameBtn.addEventListener('click', () => {
-    changeBtn()
-    timer(9)
-    createImage()
-    bgm.play();
+    changeBtn();
+    timer(9);
+    bgSound.play();
+    carrots.forEach((item) => {
+        let randomX = Math.floor((Math.random() * itemsWidth));
+        let randomY = Math.floor((Math.random() * itemsHeight));
+        item.style.left = randomX + 'px';
+        item.style.top = randomY + 'px';
+    })
+
+    bugs.forEach((item) => {
+        let randomX = Math.floor((Math.random() * itemsWidth));
+        let randomY = Math.floor((Math.random() * itemsHeight));
+        item.style.left = randomX + 'px';
+        item.style.top = randomY + 'px';
+    })
 })
