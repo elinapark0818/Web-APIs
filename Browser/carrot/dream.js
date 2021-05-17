82ea0a28c495a7479223d721ff34f44e7812d579
ElinaPark <elinapark0818@gmail.com>
2021-05-17 23:27:15 +0900

    당근, 벌레 개수를 선언

--- Browser/carrot/dream.js
+++ Browser/carrot/dream.js
@@ -1,14 +1,17 @@
 'use strict';
 
 const CARROT_SIZE = 80;
+const CARROT_COUNT = 5;
+const BUG_COUNT = 5;
+
 const field = document.querySelector('.game__field');
 const fieldRect = field.getBoundingClientRect();
 
 function initGame() {
 //    벌레와 당근을 생성한 뒤, field 에 추가한다
     console.log(fieldRect);
-    addItem('carrot', 5, 'img/carrot.png');
-    addItem('bug', 5, 'img/bug.png');
+    addItem('carrot', CARROT_COUNT, 'img/carrot.png');
+    addItem('bug', BUG_COUNT, 'img/bug.png');
 }
 
 function addItem(className, count, imgPath) {
@@ -32,6 +35,4 @@ function addItem(className, count, imgPath) {
 function randomNumber(min, max) {
     return Math.random() * (max - min) + min;
 }
-
-
 initGame();
