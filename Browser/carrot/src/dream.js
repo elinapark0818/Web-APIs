8dc4c0652bf84b73341f86025c55d07290a6ef9e
ElinaPark <elinapark0818@gmail.com>
2021-05-30 20:26:23 +0900

    sound 옮기기

--- Browser/carrot/src/dream.js
+++ Browser/carrot/src/dream.js
@@ -2,6 +2,7 @@
 
 import PopUp from './popup.js';
 import Field from './field.js';
+import * as sound from './sound.js';
 
 
 const CARROT_COUNT = 5;
@@ -9,18 +10,9 @@ const BUG_COUNT = 5;
 const GAME_DURATION_SEC = 5;
 
 
-
 const gameBtn = document.querySelector('.game__btn');
 const gameTimer = document.querySelector('.game__timer');
 const gameScore = document.querySelector('.game__score');
-
-
-const carrotSound = new Audio('./sound/carrot_pull.mp3');
-const alertSound = new Audio('./sound/alert.wav');
-const bgSound = new Audio('./sound/bg.mp3');
-const bugSound = new Audio('./sound/bug_pull.mp3');
-const winSound = new Audio('./sound/game_win.mp3');
-
 
 let started = false;
 let score = 0;
@@ -139,15 +131,6 @@ function initGame() {
     gameField.init();
 }
 
-
-function playSound(sound) {
-    sound.currentTime = 0;
-    sound.play();
-}
-
-function stopSound(sound) {
-    sound.pause();
-}
 
 function updateScoreBoard() {
     gameScore.innerText = CARROT_COUNT - score;
