045fdd16bbd6d5b25c5692ec3e9b4de23a5171f4
ElinaPark <elinapark0818@gmail.com>
2021-05-30 21:53:25 +0900

    this 바인딩 event => this.onClick(event)

--- Browser/carrot/src/field.js
+++ Browser/carrot/src/field.js
@@ -11,7 +11,7 @@ export default class Field {
         this.bugCount = bugCount;
         this.field = document.querySelector('.game__field');
         this.fieldRect = this.field.getBoundingClientRect();
-        this.field.addEventListener('click', this.onClick);
+        this.field.addEventListener('click', event => this.onClick(event));
     }
 
     init() {
@@ -43,7 +43,7 @@ export default class Field {
         }
     }
 
-    onClick = (event) => {
+    onClick(event) {
         const target = event.target;
         if (target.matches('.carrot')) {
             target.remove();
@@ -51,6 +51,7 @@ export default class Field {
             this.onItemClick && this.onItemClick('carrot');
         } else if (target.matches('.bug')) {
             this.onItemClick && this.onItemClick('bug');
+
         }
     }
 }
