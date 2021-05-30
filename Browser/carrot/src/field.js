f988cd8c0ca4a5b6a2a5ee376f6bee4eafed4f65
ElinaPark <elinapark0818@gmail.com>
2021-05-30 19:43:58 +0900

    init, addItem 옮기기

--- Browser/carrot/src/field.js
+++ Browser/carrot/src/field.js
@@ -9,4 +9,31 @@ export default class Field {
         this.field.addEventListener('click', this.onClick);
     }
 
+    init() {
+        field.innerHTML = '';
+        addItem('carrot', CARROT_COUNT, 'img/carrot.png');
+        addItem('bug', BUG_COUNT, 'img/bug.png');
+    }
+
+    addItem(className, count, imgPath) {
+        const x1 = 0;
+        const y1 = 0;
+        const x2 = fieldRect.width - CARROT_SIZE;
+        const y2 = fieldRect.height - CARROT_SIZE;
+        for (let i = 0; i < count; i++) {
+            const item = document.createElement('img');
+            item.setAttribute('class', className);
+            item.setAttribute('src', imgPath);
+            item.style.position = 'absolute';
+            const x = randomNumber(x1, x2);
+            const y = randomNumber(y1, y2);
+            item.style.left = `${x}px`;
+            item.style.top = `${y}px`;
+            field.appendChild(item);
+        }
+    }
+
+    onClick(event) {
+
+    }
 }
